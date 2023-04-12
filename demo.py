cb94339f358e6530cd6f30180cedb9266139259b
nate stemen <nate@unitary.fund>
2023-04-12 13:45:36 -0700

    pre mitiq

---
+++ demo.py
@@ -0,0 +1,18 @@
+def execute(circuit, noise_level=0.005):
+    """Returns Tr[ρ |0⟩⟨0|] where ρ is the state prepared by the circuit
+    executed with depolarizing noise.
+    """
+    noisy_circuit = circuit.with_noise(cirq.depolarize(p=noise_level))
+    rho = cirq.DensityMatrixSimulator().simulate(noisy_circuit).final_density_matrix
+    return rho[0, 0].real
+
+
+import cirq
+
+qubit = cirq.LineQubit(1)
+circuit = cirq.Circuit(cirq.X(qubit) for _ in range(100))
+
+expval = execute(circuit)
+
+print(f"Error: {1 - expval:.3}")
+# Error: 0.244
