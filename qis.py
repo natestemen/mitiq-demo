afcbe697506e2ff13b261a9f8ae4e18578f5fc72
nate stemen <nate@unitary.fund>
2023-04-12 16:02:59 -0700

    add qiskit version

---
+++ qis.py
@@ -0,0 +1,28 @@
+from qiskit_aer.noise import NoiseModel, depolarizing_error
+from qiskit_aer import AerSimulator
+import numpy as np
+
+
+def execute(circuit):
+    c = circuit.copy()
+    c.save_density_matrix()
+
+    noise_model = NoiseModel()
+    error = depolarizing_error(0.005, 1)
+    noise_model.add_all_qubit_quantum_error(error, ["x", "u1", "u2", "u3"])
+    backend = AerSimulator(method="density_matrix", noise_model=noise_model)
+    rho = backend.run(c).result().data()["density_matrix"]
+    return np.asarray(rho)[0, 0].real
+
+
+import qiskit
+
+circuit = qiskit.QuantumCircuit(1)
+for _ in range(100):
+    circuit.x(0)
+circuit.measure_all()
+
+expval = execute(circuit)
+
+print(f"Error: {1 - expval:.3}")
+# Error: 0.214
